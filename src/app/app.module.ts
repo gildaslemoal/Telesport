915bdce8c1bacb97ec4556c32c2e0fb5d7e446e6
gildaslemoal <9318266+gildaslemoal@users.noreply.github.com>
2024-12-11 11:26:15 +0100

    Config client http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,8 +8,8 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideHttpClient(),],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
